38aaeb2eb853b4ec37b81f1d0ebc40ea840977df
gulrehman16 <gulrehman05@gmail.com>
2022-06-16 16:14:48 +0500

    for changes
    for changes

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import MyStack from './src/navigation/stack';
-import FeaturedPost from './src/screens/homes/FeaturedPost';
+import StoreCreate from './src/screens/homes/Storecreate';
 import Productdetails from './src/screens/homes/ProductDetails';
-import Comment from './src/components/comment';
 const App = props => {
-  // return <MyStack {...props} />;
-  return <Productdetails {...props} />;
+  return <MyStack {...props} />;
+  // return <Productdetails {...props} />;
 };
 
 export default App;
